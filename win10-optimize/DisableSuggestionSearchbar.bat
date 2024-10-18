78447bc9a2b8e1e813fe3196b65033b8ac535bc8
tianmuyun <127042289+tianmuyun@users.noreply.github.com>
2024-10-18 14:39:09 +0800

    Delete win10-optimize directory

--- win10-optimize/DisableSuggestionSearchbar.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001
-
-:: 以管理员身份运行
-
-echo 禁用 Windows 搜索框中的建议功能
-reg add "HKEY_CURRENT_USER\SOFTWARE\Policies\Microsoft\Windows\explorer" /v DisableSearchBoxSuggestions /t reg_dword /d 1 /f
-pause
